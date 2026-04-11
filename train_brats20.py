cc7caa8bd5992d32c13d2f791b7b2b2dae550dab
Ahmed Marouane Djouama <ahmedmarouane.d@gmail.com>
2026-04-11 13:09:47 +0100

    log opt and model device

--- train_brats20.py
+++ train_brats20.py
@@ -490,18 +490,18 @@ def main():
         print(f'Number of items in val_dataset: {len(val_dataset):,}')
 
 
-    weights = compute_slice_weights(case_dirs_train, slice_range=dataset_config['slice_range'], nonzero_weight=5.0, empty_weight=1.0)
+    # weights = compute_slice_weights(case_dirs_train, slice_range=dataset_config['slice_range'], nonzero_weight=5.0, empty_weight=1.0)
     
-    sampler = WeightedRandomSampler(
-        weights=weights,
-        num_samples=len(weights),   # one full epoch worth of draws
-        replacement=True,           # required for weighted sampling
-        generator=sampler_gen,
-    )
+    # sampler = WeightedRandomSampler(
+    #     weights=weights,
+    #     num_samples=len(weights),   # one full epoch worth of draws
+    #     replacement=True,           # required for weighted sampling
+    #     generator=sampler_gen,
+    # )
 
 
     train_dl = data.DataLoader(
-        train_dataset, args.batch_size, sampler=sampler,
+        train_dataset, args.batch_size, shuffle=True,
         num_workers=args.num_workers, persistent_workers=True, pin_memory=True, generator=dl_gen, worker_init_fn=worker_init_fn
     )
 
@@ -653,6 +653,14 @@ def main():
     else:
         epoch = 0
         step = 0
+
+    # log model device and opt device
+    if accelerator.is_main_process:
+        model_device = next(inner_model.parameters()).device
+        opt_device = opt.param_groups[0]['params'][0].device
+        print(f'Model device: {model_device}, Optimizer device: {opt_device}', flush=True)
+    
+    exit(0)
 
     # Metrics logging
     evaluate_enabled = eval_every > 0 and args.evaluate_n > 0
